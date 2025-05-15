8051a94a1e56a5df8a396c405f9c608f807bfb16
z31nnx <zeistnn@gmail.com>
2025-05-14 16:30:57 -1000

    Another small update...

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,16 +9,16 @@ terraform {
 
 locals {
   global_tags = {
-    Environment = var.environment
-    Project = var.project
-    Owner = var.owner
-    ManagedBy = var.managedby
+    Environment = "Dev"
+    Project = "Testing"
+    Owner = "Test"
+    ManagedBy = "Terraform"
   }
 }
 
 module "ec2_stack" {
   source = "../../modules/ec2-stack"
-  ec2_name = var.ec2_name
-  ami_names = var.ami_names
+  ec2_name = "web"
+  ami_names = "amazon-linux-ami-2"
   tags = local.global_tags
 }
