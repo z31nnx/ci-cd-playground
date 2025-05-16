de5b6b4b86447927f2d6e1c1e286fd199ad1cf33
z31nnx <zeistnn@gmail.com>
2025-05-15 20:58:11 -1000

    Fixed ?

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,6 +19,6 @@ locals {
 module "ec2_stack" {
   source    = "../../modules/ec2-stack"
   ec2_name  = var.ec2_name
-  ami_names = var.ami_name
+  ami_name = var.ami_name
   tags      = local.global_tags
 }
